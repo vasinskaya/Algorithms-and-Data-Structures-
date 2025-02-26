a1a3f0fd421a5f5332ad70baca9856ad2d261e7e
vasinskaya <semenmary1@gmail.com>
2025-02-26 20:04:17 +0700

    Add files via upload

---
+++ seminars/practice 1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = SumOfDigitOfNumbers(1, 100);
+            Console.WriteLine("Hello World!");
+        }
+        static int SumOfDigitOfNumbers(int left, int right)
+        {
+            int sum = 0;
+            for(int number = left; number <= right; number++)
+            {
+                int tmp = number;
+                while (tmp>0)
+                {
+                    if (tmp % 2 == 0)
+                        sum += tmp % 10;
+                    tmp /= 10;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ seminars/practice 2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();                
+            ConsoleKeyInfo tmp;
+            int max = 0;
+            int min = 0;
+            int attempt = 0;
+            int gamesCount = 0;
+            do
+            {
+                int number = random.Next(1, 101);
+                gamesCount++;
+                int currentAttempt = 0;
+
+                while (true)
+                {
+                    
+                    currentAttempt++;
+                    int mysteryNumber = 0;
+                    Console.WriteLine("Enter mystery number in [1, 100]");
+                    while (!int.TryParse(Console.ReadLine(), out mysteryNumber)
+                        || mysteryNumber < 1 || mysteryNumber > 100)
+                        Console.WriteLine("Error. Enter correct number in [1, 100]");
+                    if (mysteryNumber > number)
+                        Console.WriteLine("Guessed number is less");
+                    else if (mysteryNumber < number)
+                        Console.WriteLine("Guessed number is bigger");
+                    else
+                    {
+                        Console.WriteLine("You won!");
+                        break;
+                    }
+                }
+                attempt += currentAttempt;
+                min = min == 0 || min > currentAttempt 
+                    ? currentAttempt 
+                    : min;
+                //if (min > attemt) min = attempt
+                max = max < attempt ? attempt : max;
+                tmp = Console.ReadKey();
+            } while (tmp.Key == ConsoleKey.Y);
+            Console.WriteLine($"Min = {min}\n" +
+                $"Max = {max}\nAvg = {Convert.ToDouble(attempt)/gamesCount}");
+        }
+    }
+}
